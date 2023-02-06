3be66d1f03bdbe8668cd1edc5fa2f954ef662f1c
Bartek Krawczyk <bartoszkrawczykk@gmail.com>
2023-02-06 19:06:20 +0100

    Rozwiązanie 2

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,10 +3,7 @@ package pl.javastart.task;
 public class Main {
 
     public static void main(String[] args) {
-        Table table1 = new Table();
-        table1.book = "Harry Potter";
-        table1.pen = "Black";
-
-        System.out.println(table1.book + " " + table1.pen);
+        Table table = new Table("Harry Potter", "Black");
+        table.printInfo();
     }
 }
--- src/main/java/pl/javastart/task/Table.java
+++ src/main/java/pl/javastart/task/Table.java
@@ -4,4 +4,13 @@ public class Table {
     String pen;
     String book;
     String lamp;
+
+    Table(String pen1, String book1) {
+        pen = pen1;
+        book = book1;
+    }
+
+    void printInfo() {
+        System.out.println(pen + " " + book);
+    }
 }
